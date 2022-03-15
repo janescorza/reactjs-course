81b2a53d8ad5fa058de0f6ca6e2303d93f345f09
Jan <jan.escorza99@gmail.com>
2022-03-15 12:28:11 +0100

    adding a button with svg icon & spans

--- section-11-project/src/components/Layout/Header.js
+++ section-11-project/src/components/Layout/Header.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import styles from './Header.module.css';
-import mealsImage from "../../assets/meals.jpg";
+import mealsImage from '../../assets/meals.jpg';
+import CartButton from './HeaderCartButton' 
 
 const Header = (props) => {
+
+    const clickHandler = (event) => {
+        console.log("Button has been clicked");
+    }
 
     return(
         <React.Fragment>
             <header className={styles.header}>
                 <h1>ReactMeals</h1>
-                <button>Cart</button>
+                <CartButton
+                onClick={clickHandler}
+                />
             </header>
             <div className={styles['main-image']}>
                 <img src={mealsImage} alt="" />
---
+++ section-11-project/src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,19 @@
+import React from "react";
+import CartIcon from "../Cart/CartIcon";
+import styles from "./HeaderCartButton.module.css";
+
+const HeaderCartButton = (props) => {
+  return (
+    <button className={styles.button} onClick={props.onClick}>
+      <span className={styles.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={styles.badge}>
+        1
+      </span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
