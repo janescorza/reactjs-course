4de93064039befd85d53f58a01e7d2910eaa4499
Jan <jan.escorza99@gmail.com>
2022-03-16 22:54:15 +0100

    Adding bump animation with useEffect and setTimeout + clean up function

--- section-11-project/src/components/Layout/HeaderCartButton.js
+++ section-11-project/src/components/Layout/HeaderCartButton.js
@@ -1,25 +1,40 @@
-import React , {useContext} from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 import styles from "./HeaderCartButton.module.css";
-import CartContext from '../../store/cart-context';
-
+import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
-  
-  
-  const cartContext = useContext(CartContext);//Header button component will be RE-evaluated whenever the context is updated
+  const cartContext = useContext(CartContext); //Header button component will be RE-evaluated whenever the context is updated
+  const [btnIsHighlighted,setBtnIsHighlighted] =useState(false);
 
-  const numberOfCartItems = cartContext.items.reduce((total, item) => {return total + item.amount},0)//<<<<<<<<<<<<<------------------
-    
+  const {items} = cartContext;
+  const numberOfCartItems = items.reduce((total, item) => {
+    return total + item.amount;
+  }, 0); //<<<<<<<<<<<<<------------------
+
+  let btnClasses = `${styles.button} ${btnIsHighlighted ? styles.bump : ""}`;
+
+
+   
+  useEffect(() => {
+    if(cartContext.items.length === 0) return;
+    setBtnIsHighlighted(true)
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    },300)
+
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [items]);
+
   return (
-    <button className={styles.button} onClick={props.onShowCart}>
+    <button className={btnClasses} onClick={props.onShowCart}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={styles.badge}>
-        {numberOfCartItems}
-      </span>
+      <span className={styles.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
