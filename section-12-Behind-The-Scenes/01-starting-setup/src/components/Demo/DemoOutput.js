e34765c17d86882a3ce37cfaba08bbedb1defcea
Jan <jan.escorza99@gmail.com>
2022-03-17 10:17:49 +0100

    Adding demo to see how it stops re-rendering

--- section-12-Behind-The-Scenes/01-starting-setup/src/components/Demo/DemoOutput.js
+++ section-12-Behind-The-Scenes/01-starting-setup/src/components/Demo/DemoOutput.js
@@ -3,6 +3,6 @@ import React from 'react';
 const DemoOutput = (props) => {
   console.log('DemoOutput RUNNING');
   return <p>{props.show ? 'This is new!' : ''}</p>;
-};
+}; 
 
-export default DemoOutput;
+export default React.memo(DemoOutput);
