3dde9e41a623ea6a2d01ea19df9c9d778665e4d7
Jan <jan.escorza99@gmail.com>
2022-03-18 22:30:25 +0100

    completed custom hook but pending to revise vids

--- section-15-customHooks/04-onwards-to-a-more-realistic-example/src/App.js
+++ section-15-customHooks/04-onwards-to-a-more-realistic-example/src/App.js
@@ -2,48 +2,29 @@ import React, { useEffect, useState } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
-import useApi from './hooks/use-api';
+import useHttp from './hooks/use-http';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const FetchTasks = async (taskText) => {
-    const [isLoadingValue, errorValue, loadedTasks] = useApi(taskText,false);
-    setIsLoading(isLoadingValue);
-    setError(errorValue);
-    setTasks(loadedTasks);
-  //   setIsLoading(true);
-  //   setError(null);
-  //   try {
-  //     const response = await fetch(
-  //       'https://react-js-course-b4b3c-default-rtdb.firebaseio.com/tasks.json',{}
-  //       // 'https://react-http-6b4a6.firebaseio.com/tasks.json'
-  //     );
-
-  //     if (!response.ok) {
-  //       throw new Error('Request failed!');
-  //     }
-
-  //     const data = await response.json();
-
-  //     const loadedTasks = [];
-
-  //     for (const taskKey in data) {
-  //       loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-  //     }
-
-  //     setTasks(loadedTasks);
-  //   } catch (err) {
-  //     setError(err.message || 'Something went wrong!');
-  //   }
-  //   setIsLoading(false);
-  };
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
   useEffect(() => {
-    FetchTasks();
-  }, []);
+    const transformTasks = (tasksObj) => {
+      const loadedTasks = [];
+
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    };
+
+    fetchTasks(
+      { url: 'https://react-js-course-b4b3c-default-rtdb.firebaseio.com/tasks.json' },
+      transformTasks
+    );
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
@@ -56,7 +37,7 @@ function App() {
         items={tasks}
         loading={isLoading}
         error={error}
-        onFetch={FetchTasks}
+        onFetch={fetchTasks}
       />
     </React.Fragment>
   );
--- section-15-customHooks/04-onwards-to-a-more-realistic-example/src/components/NewTask/NewTask.js
+++ section-15-customHooks/04-onwards-to-a-more-realistic-example/src/components/NewTask/NewTask.js
@@ -1,52 +1,34 @@
-import { useState } from 'react';
-
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
-import useApi from '../../hooks/use-api'
+import useHttp from '../../hooks/use-http';
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { isLoading, error, sendRequest: sendTaskRequest } = useHttp();
 
+  const createTask = (taskText, taskData) => {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
 
-  const EnterTaskHandler = async (taskText) => {
-    const [isLoadingValue, errorValue,createdTask] = useApi(taskText);
-    setIsLoading(isLoadingValue);
-    setError(errorValue);
     props.onAddTask(createdTask);
-    // setIsLoading(true);
-    // setError(null);
-    // try {
-    //   const response = await fetch(
-    //     'https://react-js-course-b4b3c-default-rtdb.firebaseio.com/tasks.json',
-    //     {
-    //       method: 'POST',
-    //       body: JSON.stringify({ text: taskText }),
-    //       headers: {
-    //         'Content-Type': 'application/json',
-    //       },
-    //     }
-    //   );
+  };
 
-    //   if (!response.ok) {
-    //     throw new Error('Request failed!');
-    //   }
-
-    //   const data = await response.json();
-
-    //   const generatedId = data.name; // firebase-specific => "name" contains generated id
-    //   const createdTask = { id: generatedId, text: taskText };
-
-    //   props.onAddTask(createdTask);
-    // } catch (err) {
-    //   setError(err.message || 'Something went wrong!');
-    // }
-    // setIsLoading(false);
+  const enterTaskHandler = async (taskText) => {
+    sendTaskRequest(
+      {
+        url: 'https://react-js-course-b4b3c-default-rtdb.firebaseio.com/tasks.json',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: { text: taskText },
+      },
+      createTask.bind(null, taskText)
+    );
   };
 
   return (
     <Section>
-      <TaskForm onEnterTask={EnterTaskHandler} loading={isLoading} />
+      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
       {error && <p>{error}</p>}
     </Section>
   );
