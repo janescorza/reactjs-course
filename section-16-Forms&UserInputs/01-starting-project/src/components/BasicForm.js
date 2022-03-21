c1edefc542bb2c69b5aaf8d1eae182b39a6cd489
Jan <jan.escorza99@gmail.com>
2022-03-21 16:12:53 +0100

    My 1st try on basicForm prehook

--- section-16-Forms&UserInputs/01-starting-project/src/components/BasicForm.js
+++ section-16-Forms&UserInputs/01-starting-project/src/components/BasicForm.js
@@ -1,35 +1,64 @@
-import {useState} from 'react';
+import { useState } from "react";
 
 const BasicForm = (props) => {
-  const [enteredName, setEnteredName] = useState('');
-  // const [nameIsValid, setNameIsValid] = useState(false);
+  const [enteredName, setEnteredName] = useState("");
   const [nameIsTouched, setNameIsTouched] = useState(false);
-  
 
-  const nameIsValid = enteredName.trim() !== '';
+  const nameIsValid = enteredName.trim() !== "";
   const nameHasError = !nameIsValid && nameIsTouched;
+  const nameInputClassNames = nameHasError
+    ? "form-control invalid"
+    : "form-control";
 
-  const nameInputClassNames = namehasError ? 'form-control invalid' : 'form-control';
+  const nameChangeHandler = (event) => {
+    console.log("Name changed to: " + event.target.value);
+    setEnteredName(event.target.value);
+  };
+  const nameBlurHandler = () => {
+    setNameIsTouched(true);
+  };
+
+  const validateForm = (event) => {
+    event.preventDefault();
+    setNameIsTouched(true);
+
+    if (nameHasError) {
+      return;
+    }
+    console.log("The name is: " + enteredName);
+
+    setNameIsTouched(false);
+    setEnteredName("");
+  };
+
+  let formIsValid = false;
+  if (nameIsValid) formIsValid = true;
 
   return (
-    <form>
-      <div className='control-group'>
+    <form onSubmit={validateForm}>
+      <div className="control-group">
         <div className={nameInputClassNames}>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+          <label htmlFor="name">First Name</label>
+          <input
+            type="text"
+            id="name"
+            onChange={nameChangeHandler}
+            onBlur={nameBlurHandler}
+            value={enteredName}
+          />
           {nameHasError && <p>Name must not be empty</p>}
         </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+        <div className="form-control">
+          <label htmlFor="name">Last Name</label>
+          <input type="text" id="name" />
         </div>
       </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+      <div className="form-control">
+        <label htmlFor="name">E-Mail Address</label>
+        <input type="text" id="name" />
       </div>
-      <div className='form-actions'>
-        <button>Submit</button>
+      <div className="form-actions">
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
