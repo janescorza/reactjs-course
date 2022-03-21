0d998a083a2297bbd6232e6fccaa094d7b2c3cab
Jan <jan.escorza99@gmail.com>
2022-03-21 11:01:49 +0100

    simple UseState implementation

--- section-16-Forms&UserInputs/01-starting-project/src/components/SimpleInput.js
+++ section-16-Forms&UserInputs/01-starting-project/src/components/SimpleInput.js
@@ -1,9 +1,23 @@
+import {useRef, useState} from 'react';
+
 const SimpleInput = (props) => {
+   const [enteredName, setEnteredName] = useState('');
+   
+   const userNameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  }
+  
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+    console.log(enteredName);
+     
+   }
+
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' onChange={userNameInputChangeHandler} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
