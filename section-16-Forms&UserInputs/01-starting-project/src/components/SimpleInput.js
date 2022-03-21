9785aa0a49b2f7cff6d8b4cea28ab6ed6062d3e9
Jan <jan.escorza99@gmail.com>
2022-03-21 13:06:28 +0100

    having two input fields but duplicate code

--- section-16-Forms&UserInputs/01-starting-project/src/components/SimpleInput.js
+++ section-16-Forms&UserInputs/01-starting-project/src/components/SimpleInput.js
@@ -1,41 +1,76 @@
 import { useState, useEffect } from "react";
+const validateEmail = (email) => {
+  console.log("validating");
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
 
 const SimpleInput = (props) => {
-  // const namedInputRef = useRef();
+  //Name
   const [enteredName, setEnteredName] = useState("");
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+  //Email Address
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredEmailTouched, setEnteredEmailTouched] = useState(false);
 
-  //This will rexecute every time a new value is entred so ti always has the latest value
+  //Name validation
   const enteredNameIsValid = enteredName.trim() !== "";
   const nameInputIsInValid = !enteredNameIsValid && enteredNameTouched;
+
+  //Email validation
+  const enteredEmailIsValid = enteredEmail.trim() !== "" &&  validateEmail(enteredEmail);
+  const emailInputIsInValid = !enteredEmailIsValid && enteredEmailTouched;
+
+  //Full form validation
   let formIsValid = false;
-  if (enteredNameIsValid) {
+  if (enteredNameIsValid && enteredEmailIsValid) {
     formIsValid = true;
   }
-
+  //Name state functions
   const userNameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
   };
-
   const userNameInputBlurHandler = (event) => {
     setEnteredNameTouched(true);
+  };
+
+  //Email state functions
+  const emailInputChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+  const emailInputBlurHandler = (event) => {
+    setEnteredEmailTouched(true);
   };
 
   const formSubmissionHandler = (event) => {
     event.preventDefault();
 
     setEnteredNameTouched(true);
+    setEnteredEmailTouched(true);
 
-    if (!enteredNameIsValid) {
+    if (!enteredNameIsValid || !enteredEmailIsValid) {
       return;
     }
-    console.log(enteredName);
-
+    console.log(enteredName + enteredEmail);
+    //name reset
     setEnteredName("");
     setEnteredNameTouched(false);
+
+    //email reset
+    setEnteredEmail("");
+    setEnteredEmailTouched(false);
   };
 
+  //name class modification
   const nameInputClasses = nameInputIsInValid
+    ? "form-control invalid"
+    : "form-control";
+
+  //email class modification
+  const emailInputClasses = emailInputIsInValid
     ? "form-control invalid"
     : "form-control";
 
@@ -53,6 +88,20 @@ const SimpleInput = (props) => {
         />
         {nameInputIsInValid && (
           <p className="error-text">Name must not be empty!</p>
+        )}
+      </div>
+      <div className={emailInputClasses}>
+        <label htmlFor="email">Your Email</label>
+        <input
+          type="email"
+          id="email"
+          onChange={emailInputChangeHandler}
+          onBlur={emailInputBlurHandler}
+          value={enteredEmail}
+          // ref={namedInputRef}
+        />
+        {emailInputIsInValid && (
+          <p className="error-text">The entred email must be valid!</p>
         )}
       </div>
       <div className="form-actions">
