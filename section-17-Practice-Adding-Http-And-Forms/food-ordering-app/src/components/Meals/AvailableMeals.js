82ab6c512437f3bb9db76f1cde120d1927124363
Jan <jan.escorza99@gmail.com>
2022-03-29 20:41:50 +0200

    adding direct fetch without custom hook

--- section-17-Practice-Adding-Http-And-Forms/food-ordering-app/src/components/Meals/AvailableMeals.js
+++ section-17-Practice-Adding-Http-And-Forms/food-ordering-app/src/components/Meals/AvailableMeals.js
@@ -5,6 +5,42 @@ import useHttp from "../../hooks/use-http";
 import { useState, useEffect } from "react";
 
 const AvailableMeals = () => {
+  //USING FETCH TO GET DATA FROM HTTP:
+  const [meals,setMeals] = useState([]);
+  useEffect(()=>{
+    const fetchMeals = async () => {
+      const response = await fetch('https://react-js-course-b4b3c-default-rtdb.firebaseio.com/meals.json');
+      const responseData = await response.json();
+      //I got an object but I want an array
+      const loadedMeals = [];
+
+      for (const mealKey in responseData) {
+        loadedMeals.push({
+          id: mealKey,
+          name: responseData[mealKey].name,
+          description: responseData[mealKey].description,
+          price: responseData[mealKey].price,
+        });
+      }
+      setMeals(loadedMeals);
+      
+    }
+    fetchMeals();
+  },[]);//No dependencies so it runs only on load
+
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+  
+  
+  //USING A CUSTOM HTTP HOOK:
+  /*
   const [mealsList, setMealsList] = useState([]);
   const { isLoading, error, sendRequest: fetchData } = useHttp();
 
@@ -39,6 +75,7 @@ const AvailableMeals = () => {
 
     fetchData(requestConfig, transformMeals);
   }, [fetchData]);
+  */
   
 
   return (
