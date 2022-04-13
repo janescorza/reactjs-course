6187e7a4e62b74a75eb62c578c171d740b8351da
Jan <jan.escorza99@gmail.com>
2022-04-13 23:29:17 +0200

    showing item via route based on id and gettin data to show

--- section-20-routing-multipage-SPA/practicing-routing/src/pages/QuoteDetails.js
+++ section-20-routing-multipage-SPA/practicing-routing/src/pages/QuoteDetails.js
@@ -1,14 +1,24 @@
 import React from "react";
 import { Route, useParams } from "react-router-dom";
-import Comments from '../components/comments/Comments'
+import Comments from '../components/comments/Comments';
+import HighlightedQuote from '../components/quotes/HighlightedQuote'
+const DUMMY_QUOTES = [
+    {id: 'q1', author:'a', text:'abc'},
+    {id: 'q2', author:'b', text:'asw'}
+  ];
+  
 
 function QuoteDetails() {
   const params = useParams();
+  const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
+  if(!quote){
+      return <p>No quote found for that id!</p>
+  }
   return (
     <div>
       <h1>QuoteDetails</h1>
-      <h2>hey</h2>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author}/>
+      {/* <p>{params.quoteId}</p> */}
       {/* <Route path="/quotes/:quoteId/comments"> */}
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments/>
