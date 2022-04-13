252688b1347f3d9bae9c0c5cb96271b97dd18293
Jan <jan.escorza99@gmail.com>
2022-04-13 18:45:00 +0200

    Using links for unstyled and navlinks for styled links to routes

--- section-20-routing-multipage-SPA/learning-routing/src/App.js
+++ section-20-routing-multipage-SPA/learning-routing/src/App.js
@@ -1,15 +1,21 @@
 import { Route } from "react-router-dom";
-import Welcome from './pages/Welcome'
-import Products from './pages/Products'
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import MainHeader from "./components/MainHeader";
+import ProductDetail from "./pages/ProductDetail";
 
 function App() {
   return (
     <div>
+      <MainHeader />
       <Route path="/welcome">
-        <Welcome/>
+        <Welcome />
       </Route>
-      <Route path='/products'>
-        <Products/>
+      <Route path="/products">
+        <Products />
+      </Route>
+      <Route path="/products/:productId">
+        <ProductDetail />
       </Route>
     </div>
   );
--- section-20-routing-multipage-SPA/learning-routing/src/pages/Products.js
+++ section-20-routing-multipage-SPA/learning-routing/src/pages/Products.js
@@ -1,5 +1,22 @@
+import { Fragment } from "react";
+import { Link } from "react-router-dom";
 const Welcome = () => {
-    return <h1>The Products Page</h1>
-}
+  return (
+    <Fragment>
+      <h1>The Products Page</h1>
+      <ul>
+        <li>
+          <Link to="/products/p1">A Book</Link>
+        </li>
+        <li>
+          <Link to="/products/p2">A House</Link>
+        </li>
+        <li>
+          <Link to="/products/p3">A Carper</Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
+};
 
-export default Welcome
+export default Welcome;
