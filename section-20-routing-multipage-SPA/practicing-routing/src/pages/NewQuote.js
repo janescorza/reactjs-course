f540529ac9381085401ee09b7f3efa4b0f207514
Jan <jan.escorza99@gmail.com>
2022-04-14 00:27:48 +0200

    implement useHistory to programatically navigate user

--- section-20-routing-multipage-SPA/practicing-routing/src/pages/NewQuote.js
+++ section-20-routing-multipage-SPA/practicing-routing/src/pages/NewQuote.js
@@ -1,9 +1,12 @@
 import QuoteForm from '../components/quotes/QuoteForm'
+import {useHistory} from 'react-router-dom'
 
 function NewQuote() {
+    const history = useHistory(); 
 
     const addQuoteHandler = quoteData =>{
         console.log(quoteData);
+        history.push('/quotes');
     }
   return (
       <QuoteForm onAddQuote={addQuoteHandler}/>
