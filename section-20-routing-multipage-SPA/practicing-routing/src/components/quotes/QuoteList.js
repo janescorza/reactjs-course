d05ea5e230a951b73d35b524a526a0c16cd96985
Jan <jan.escorza99@gmail.com>
2022-04-14 10:48:41 +0200

    effectively sorting quotes

--- section-20-routing-multipage-SPA/practicing-routing/src/components/quotes/QuoteList.js
+++ section-20-routing-multipage-SPA/practicing-routing/src/components/quotes/QuoteList.js
@@ -3,11 +3,22 @@ import { useHistory, useLocation } from "react-router-dom";
 import QuoteItem from "./QuoteItem";
 import classes from "./QuoteList.module.css";
 
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
+
 const QuoteList = (props) => {
   const history = useHistory();
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const isSortingAscending = queryParams.get('sort') === 'asc';
+  const sortedQuotes = sortQuotes(props.quotes,isSortingAscending);
 
   const changeSortingHandler = () => {
     history.push('/quotes?sort=' + (isSortingAscending? 'desc' : 'asc'))
@@ -18,7 +29,7 @@ const QuoteList = (props) => {
         <button onClick={changeSortingHandler}>Sort {isSortingAscending? 'Ascending' : 'Descending'}</button>
       </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
+        {sortedQuotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
