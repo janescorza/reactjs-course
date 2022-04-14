ff53d2db557945b790669bc18d97dd2b90478247
Jan <jan.escorza99@gmail.com>
2022-04-14 13:30:14 +0200

    making use of setters and gettes for quotes in quotedetails/newquote/allquotes

--- section-20-routing-multipage-SPA/practicing-routing/src/pages/AllQuotes.js
+++ section-20-routing-multipage-SPA/practicing-routing/src/pages/AllQuotes.js
@@ -1,13 +1,40 @@
-import QuoteList from '../components/quotes/QuoteList'
-const DUMMY_QUOTES = [
-  {id: 'q1', author:'a', text:'abc'},
-  {id: 'q2', author:'b', text:'asw'}
-];
+import QuoteList from "../components/quotes/QuoteList";
+import { getAllQuotes } from "../lib/api";
+import { useEffect } from "react";
+import useHttp from "../hooks/use-http";
+import LoadingSpinner from '../components/UI/LoadingSpinner'
+import NoQuotesFound from '../components/quotes/NoQuotesFound'
+
+// const DUMMY_QUOTES = [
+//   { id: "q1", author: "a", text: "abc" },
+//   { id: "q2", author: "b", text: "asw" },
+// ];
 
 function AllQuotes() {
-  return (
-      <QuoteList quotes={DUMMY_QUOTES}/>
-  );
+  const {
+    sendRequest,
+    status,
+    data: loadedQuotes,
+    error,
+  } = useHttp(getAllQuotes, true);
+
+  useEffect(() => {
+    sendRequest();
+  },[sendRequest])//When the sendRequest changes which is basically only on initial load
+
+  if(status==='pending'){
+    return <div className="centered">
+      <LoadingSpinner/>
+    </div>
+  }
+  if(error){
+    return <p className="centered focused">{error}</p>
+  }
+  
+  if(status === 'completed' && (!loadedQuotes || loadedQuotes.length === 0)){
+    return <NoQuotesFound/>
+  }
+  return <QuoteList quotes={loadedQuotes} />;
 }
 
 export default AllQuotes;
--- section-20-routing-multipage-SPA/practicing-routing/src/pages/QuoteDetails.js
+++ section-20-routing-multipage-SPA/practicing-routing/src/pages/QuoteDetails.js
@@ -1,8 +1,11 @@
-import React from "react";
-import { Link, Route, useLocation, useParams, useRouteMatch } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link, Route, useParams, useRouteMatch } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
-import { useState } from "react";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
+
 const DUMMY_QUOTES = [
   { id: "q1", author: "a", text: "abc" },
   { id: "q2", author: "b", text: "asw" },
@@ -10,25 +13,41 @@ const DUMMY_QUOTES = [
 
 function QuoteDetails() {
   const params = useParams();
-  // const location = useLocation();
   const match = useRouteMatch();
-  // const [showingComments, setShowingComments] = useState(
-  //   location.pathname === `/quotes/${params.quoteId}/comments` ? true : false
-  // );
-  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
-  if (!quote) {
-    return <p>No quote found for that id!</p>;
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getSingleQuote, true); //trues as we start in loading mode
+const {quoteId} = params;//To only pass the quote id as we don't want the effect to run on every change of params
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest,quoteId]);
+
+  if(status === 'pending'){
+    return <div className="centered">
+      <LoadingSpinner/>
+    </div>
+  }
+  if(error){
+    return <p className="centered">{error}</p>
+  }
+  if(!loadedQuote.text){
+    return <p>No quote found!</p>;
   }
 
-  // const showCommentsHandler = () => {
-  //   setShowingComments((prevState) => !prevState);
-  // };
+  // const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+  // if (!quote) {
+  //   return <p>No quote found for that id!</p>;
+  // }
+
   return (
     <div>
       <h1>QuoteDetails</h1>
-      <HighlightedQuote text={quote.text} author={quote.author} />
-
-{/* How to do it with state that controls when to allow showing and when to allow hiding comments */}
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
+      {/* How to do it with state that controls when to allow showing and when to allow hiding comments */}
       {/* <div className="centered">
         <Link
           className="btn--flat"
@@ -42,14 +61,13 @@ function QuoteDetails() {
           {showingComments ? "Hide Comments" : "Show Comments"}
         </Link>
       </div> */}
-
+      console.log(quoteData);
       {/* How to do it with routing (show only if comments aren't currenlty show) */}
       <Route to={`${match.path}`} exact>
         <Link className="btn--flat" to={`${match.url}/comments`}>
           Show Comments
         </Link>
       </Route>
-
       <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
