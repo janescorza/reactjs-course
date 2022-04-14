99022db85d8e49fd3ada86147c261861496e0955
Jan <jan.escorza99@gmail.com>
2022-04-14 12:16:56 +0200

    adding quote on button click and spinner while status is pending

--- section-20-routing-multipage-SPA/practicing-routing/src/pages/NewQuote.js
+++ section-20-routing-multipage-SPA/practicing-routing/src/pages/NewQuote.js
@@ -1,16 +1,22 @@
-import QuoteForm from '../components/quotes/QuoteForm'
-import {useHistory} from 'react-router-dom'
+import QuoteForm from "../components/quotes/QuoteForm";
+import { useHistory } from "react-router-dom";
+import useHttp from "../hooks/use-http";
+import { addQuote } from "../lib/api";
+import { useEffect } from "react";
 
 function NewQuote() {
-    const history = useHistory(); 
+  const { sendRequest, status } = useHttp(addQuote);
+  const history = useHistory();
 
-    const addQuoteHandler = quoteData =>{
-        console.log(quoteData);
-        history.push('/quotes');
-    }
-  return (
-      <QuoteForm onAddQuote={addQuoteHandler}/>
-  );
+  useEffect(() => {
+    if (status === "completed") history.push("/quotes");
+  }, [status,history]);
+
+  const addQuoteHandler = (quoteData) => {
+    console.log(quoteData);
+    sendRequest(quoteData);
+  };
+  return <QuoteForm isLoading={status === 'pending'} onAddQuote={addQuoteHandler} />;
 }
 
 export default NewQuote;
