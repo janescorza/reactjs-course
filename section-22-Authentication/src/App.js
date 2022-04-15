9a3fc1161537d52e5b8b6b79b10e9500ac43f25f
Jan <jan.escorza99@gmail.com>
2022-04-15 14:16:01 +0200

    logout user & using context to prevent acces to unauth pages

--- section-22-Authentication/src/App.js
+++ section-22-Authentication/src/App.js
@@ -1,25 +1,34 @@
-import { Switch, Route } from "react-router-dom";
-
+import { Switch, Route, Redirect } from "react-router-dom";
+import { useContext } from "react";
+import AuthContext from "./store/auth-context";
 import Layout from "./components/Layout/Layout";
 import UserProfile from "./components/Profile/UserProfile";
 import AuthPage from "./pages/AuthPage";
 import HomePage from "./pages/HomePage";
 
 function App() {
+  const authCtx = useContext(AuthContext);
   return (
-      <Layout>
-        <Switch>
-          <Route path="/" exact>
-            <HomePage />
-          </Route>
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
+        {!authCtx.isLoggedIn && (
           <Route path="/auth">
             <AuthPage />
           </Route>
+        )}
+        {authCtx.isLoggedIn && (
           <Route path="/profile">
             <UserProfile />
           </Route>
-        </Switch>
-      </Layout>
+        )}
+        <Route path='*'>
+          <Redirect to='/'/>
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
