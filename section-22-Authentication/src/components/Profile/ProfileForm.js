79350b9c6b22dd617966e138d145a23acee1c9b1
Jan <jan.escorza99@gmail.com>
2022-04-15 13:29:09 +0200

    completing a password change by doing a fetch on change password with error validation

--- section-22-Authentication/src/components/Profile/ProfileForm.js
+++ section-22-Authentication/src/components/Profile/ProfileForm.js
@@ -1,17 +1,59 @@
-import classes from './ProfileForm.module.css';
+import { useRef, useContext } from "react";
+import classes from "./ProfileForm.module.css";
+import AuthContext from "../../store/auth-context";
 
 const ProfileForm = () => {
+  const enteredPassword = useRef();
+  const authCtx = useContext(AuthContext);
+  const token = authCtx.token;
+  const changePasswordHandler = (event) => {
+    event.preventDefault();
+    const passwordValue = enteredPassword.current.value;
+    //add validation here
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyCGxtkp7XHD72eo6Jp-QZ_KldREFMmpN9c",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          idToken: token,
+          password: passwordValue,
+          returnSecureToken: false,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        return response.json().then((data) => {
+          let errorMessage = "Change password failed!";
+          if (data && data.error && data.error.message) {
+            errorMessage = data.error.message;
+          }
+          throw new Error(errorMessage);
+        });
+      }
+    }).then((data)=>{
+      console.log(data);
+      //Changing the password worked
+    })
+    .catch((error) => {
+      alert(error.message);
+    })
+  };
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={changePasswordHandler}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input type="password" id="new-password" minLength='5' ref={enteredPassword} />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
       </div>
     </form>
   );
-}
+};
 
 export default ProfileForm;
