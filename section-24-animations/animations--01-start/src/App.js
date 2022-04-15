64ce0a2053aaafaf234ed82bac0355baa5532f52
Jan <jan.escorza99@gmail.com>
2022-04-16 00:01:06 +0200

    log how it changes states

--- section-24-animations/animations--01-start/src/App.js
+++ section-24-animations/animations--01-start/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Transition from "react-transition-group/Transition";
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -21,24 +22,28 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <button className="Button"
+        <button
+          className="Button"
           onClick={() =>
             this.setState((prevState) => ({ showBlock: !prevState.showBlock }))
           }
         >
           Toogle
         </button>
-        <br/>
-        {this.state.showBlock ? (
-          <div
+        <br />
+        {/* {this.state.showBlock ? ( */}
+        <Transition in={this.state.showBlock} timeout={300}>
+          {state => <p>{state}</p>}
+          {/* <div
             style={{
               backgroundColor: "red",
               width: 100,
               height: 100,
-              margin: 'auto'
+              margin: "auto",
             }}
-          ></div>
-        ) : null}
+          ></div> */}
+        </Transition>
+        {/* ) : null} */}
         {this.state.modalIsOpen ? (
           <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
         ) : null}
