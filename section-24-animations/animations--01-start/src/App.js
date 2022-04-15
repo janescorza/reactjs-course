a345c2b745fad3f19e3c0d088dd99858e3f104dc
Jan <jan.escorza99@gmail.com>
2022-04-16 00:40:14 +0200

    moving the transition logic to the modal component

--- section-24-animations/animations--01-start/src/App.js
+++ section-24-animations/animations--01-start/src/App.js
@@ -51,7 +51,7 @@ class App extends Component {
           )}
         </Transition>
         
-          {(state) => <Modal show={state} closed={this.closeModal} />}
+           <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
         {this.state.modalIsOpen ? <Backdrop show /> : null}
         <button className="Button" onClick={this.showModal}>
           Open Modal
