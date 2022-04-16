5946806a22970443589df38dec8f36e4e0c9bf2b
Jan <jan.escorza99@gmail.com>
2022-04-16 19:46:51 +0200

    now any child component can acces the context

--- section-25-replace-redux/replace-redux-01-starting-project/src/index.js
+++ section-25-replace-redux/replace-redux-01-starting-project/src/index.js
@@ -1,18 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { combineReducers, createStore } from "redux";
+// import { Provider } from "react-redux";
+// import { combineReducers, createStore } from "redux";
 import { BrowserRouter } from "react-router-dom";
+
+import ProductsProvider from "./context/products-context";
 
 import "./index.css";
 import App from "./App";
 import productReducer from "./store/reducers/products";
 
-const rootReducer = combineReducers({
-  shop: productReducer,
-});
+// const rootReducer = combineReducers({
+//   shop: productReducer,
+// });
 
-const store = createStore(rootReducer);
+// const store = createStore(rootReducer);
 
 // ReactDOM.render(
 //   <Provider store={store}>
@@ -25,10 +27,11 @@ const store = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
+  // <Provider store={store}>
+  <ProductsProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>
+  </ProductsProvider>
 );
 // root.render(<App />);
