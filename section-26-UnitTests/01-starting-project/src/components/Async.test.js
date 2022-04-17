8adbe4d54e90900cc2d53fff1d43f6af07733349
Jan <jan.escorza99@gmail.com>
2022-04-17 17:20:32 +0200

    using a mock to prevent using fetch

--- section-26-UnitTests/01-starting-project/src/components/Async.test.js
+++ section-26-UnitTests/01-starting-project/src/components/Async.test.js
@@ -3,6 +3,10 @@ import Async from './Async';
 
 describe('Async component', ()=>{
     test('renders post if request succeeds', async ()=>{
+        window.fetch = jest.fn();
+        window.fetch.mockResolvedValueOnce({
+            json: async () => [{id:'p1', title: 'first post'}]
+        })
         render(<Async/>)
         const listItemElements = await screen.findAllByRole('listitem');
         expect(listItemElements).not.toHaveLength(0);
