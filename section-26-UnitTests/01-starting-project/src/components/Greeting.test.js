79328f37fd88dae3d07ba1a68e3db1400239c2be
Jan <jan.escorza99@gmail.com>
2022-04-17 15:47:45 +0200

    two basic test with no act

--- section-26-UnitTests/01-starting-project/src/components/Greeting.test.js
+++ section-26-UnitTests/01-starting-project/src/components/Greeting.test.js
@@ -1,4 +1,26 @@
-test('test if hello worlds is rendered', ()=>{
-    // Arrange
-    rem
+import {render, screen} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Greetings from './Greeting';
+
+describe('Greeting component', ()=>{
+
+    test('test if hello worlds is rendered', ()=>{
+        // Arrange
+        render(<Greetings/>);
+        //Act
+    
+        //Assert
+        const helloWorldElement = screen.getByText('Hello World', {exact: false});
+        expect(helloWorldElement).toBeInTheDocument();
+    });
+
+    test('test if good to see you is there', ()=>{
+        // Arrange
+        render(<Greetings/>);
+        //Act
+    
+        //Assert
+        const goodToSeeYouElement = screen.getByText("It's good to see you!", {exact: true});
+        expect(goodToSeeYouElement).toBeInTheDocument();
+    })
 })
