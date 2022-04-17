5ccc02512a417ee481bb5f0b28435dd0ee4307cd
Jan <jan.escorza99@gmail.com>
2022-04-17 10:16:28 +0200

    Improved the managment of dispatchers to stop them from rerendering

--- section-25-replace-redux/replace-redux-01-starting-project/src/components/Products/ProductItem.js
+++ section-25-replace-redux/replace-redux-01-starting-project/src/components/Products/ProductItem.js
@@ -1,39 +1,39 @@
-import React, {useContext} from 'react';
-import { useDispatch } from 'react-redux';
-import { useStore } from '../../hooks-store/store';
+import React, { useContext } from "react";
+import { useDispatch } from "react-redux";
+import { useStore } from "../../hooks-store/store";
 
-import Card from '../UI/Card';
-import './ProductItem.css';
+import Card from "../UI/Card";
+import "./ProductItem.css";
 // import { toggleFav } from '../../store/actions/products';
 // import { ProductsContext } from '../../context/products-context';
 
-const ProductItem = props => {
+const ProductItem = React.memo((props) => {
   // const dispatch = useDispatch();
 
   // const toggleFav = useContext(ProductsContext).toggleFav;
-  
-const dispatch = useStore()[1];
+
+  const dispatchChange = useStore(false)[1];
 
   const toggleFavHandler = () => {
     // dispatch(toggleFav(props.id));
     // toggleFav(props.id);
-    dispatch('TOGGLE_FAV',props.id);
+    dispatchChange("TOGGLE_FAV", props.id);
   };
 
   return (
-    <Card style={{ marginBottom: '1rem' }}>
+    <Card style={{ marginBottom: "1rem" }}>
       <div className="product-item">
-        <h2 className={props.isFav ? 'is-fav' : ''}>{props.title}</h2>
+        <h2 className={props.isFav ? "is-fav" : ""}>{props.title}</h2>
         <p>{props.description}</p>
         <button
-          className={!props.isFav ? 'button-outline' : ''}
+          className={!props.isFav ? "button-outline" : ""}
           onClick={toggleFavHandler}
         >
-          {props.isFav ? 'Un-Favorite' : 'Favorite'}
+          {props.isFav ? "Un-Favorite" : "Favorite"}
         </button>
       </div>
     </Card>
   );
-};
+});
 
 export default ProductItem;
--- section-25-replace-redux/replace-redux-01-starting-project/src/hooks-store/store.js
+++ section-25-replace-redux/replace-redux-01-starting-project/src/hooks-store/store.js
@@ -5,36 +5,38 @@ let globalState = {},
   listeners = [],
   actions = {};
 
-export const useStore = () => {
+export const useStore = (shouldListen = true) => {
   const setState = useState(globalState)[1]; //Get jsut teh second value
 
-const dispatch = (actionIdentifier,payload) =>{
+  const dispatch = (actionIdentifier, payload) => {
     //Calling one of the stored possible actions to alter the state
     //The actions are identified by id and here we run it ->()
-    const newState = actions[actionIdentifier](globalState,payload);
-    globalState={...globalState, ...newState}
-    for(const listener of listeners){
-        listener(globalState);
+    const newState = actions[actionIdentifier](globalState, payload);
+    globalState = { ...globalState, ...newState };
+    for (const listener of listeners) {
+      listener(globalState);
     }
-}
+  };
 
   useEffect(() => {
-    listeners.push(setState); 
+    if (shouldListen) {
+      listeners.push(setState);
+    }
     //Register a listener for the componenent when it mounts
-    //The value of set state is captured here as a closure for 
+    //The value of set state is captured here as a closure for
     //that component that's using my custom hook and there for will be the same when the component unmounts
     return () => {
-      listeners = listeners.filter((li) => li !== setState);
+      if (shouldListen) {
+        listeners = listeners.filter((li) => li !== setState);
+      }
     };
   }, [setState]);
-  return [globalState, dispatch]
+  return [globalState, dispatch];
 };
 
-export const initStore = (userActions, initialState)=>{
-    if(initialState){
-        globalState = {...globalState, ...initialState}
-    }
-    actions = {...actions, ...userActions};
-
-}
-
+export const initStore = (userActions, initialState) => {
+  if (initialState) {
+    globalState = { ...globalState, ...initialState };
+  }
+  actions = { ...actions, ...userActions };
+};
