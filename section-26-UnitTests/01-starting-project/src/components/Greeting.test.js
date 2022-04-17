13acfbf3d7b1aedaa81902ec3cf157d60f48b2b6
Jan <jan.escorza99@gmail.com>
2022-04-17 16:02:41 +0200

    many tests for posititve and negative scenarios

--- section-26-UnitTests/01-starting-project/src/components/Greeting.test.js
+++ section-26-UnitTests/01-starting-project/src/components/Greeting.test.js
@@ -1,5 +1,6 @@
 import {render, screen} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import userEvent from '@testing-library/user-event'
 import Greetings from './Greeting';
 
 describe('Greeting component', ()=>{
@@ -18,9 +19,38 @@ describe('Greeting component', ()=>{
         // Arrange
         render(<Greetings/>);
         //Act
-    
+
         //Assert
         const goodToSeeYouElement = screen.getByText("It's good to see you!", {exact: true});
         expect(goodToSeeYouElement).toBeInTheDocument();
+    })
+    test('test if good to see you is not there on button click', ()=>{
+        // Arrange
+        render(<Greetings/>);
+        //Act
+        const buttonElement = screen.getByRole('button');
+        userEvent.click(buttonElement);
+        //Assert
+        const goodToSeeYouElement = screen.queryByText("It's good to see you!", {exact: false});
+        expect(goodToSeeYouElement).not.toBeInTheDocument();
+    })
+    test('change text is not there there initially', ()=>{
+        // Arrange
+        render(<Greetings/>);
+        //Act
+    
+        //Assert
+        const changeTextElement = screen.queryByText("Changed", {exact: true});
+        expect(changeTextElement).not.toBeInTheDocument();
+    })
+    test('changed is there if button is clicked', ()=>{
+        // Arrange
+        render(<Greetings/>);
+        //Act
+        const buttonElement = screen.getByRole('button');
+        userEvent.click(buttonElement);
+        //Assert
+        const changedElement = screen.getByText("Changed", {exact: true});
+        expect(changedElement).toBeInTheDocument();
     })
 })
