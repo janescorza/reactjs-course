e0726bde4208ec7e4893f3eb161848c29c40fd0f
Jan <jan.escorza99@gmail.com>
2022-04-18 07:58:24 +0200

    incorporate call to newTodo in the main app.js

--- section-27-typescript/07-react-ts-starting-project/my-ts-app/src/App.tsx
+++ section-27-typescript/07-react-ts-starting-project/my-ts-app/src/App.tsx
@@ -1,15 +1,20 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
 import NewTodo from "./components/NewTodo";
 
-
 function App() {
-  const todos = [new Todo("LearnREAct"), new Todo("lerolero")];
-
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+    setTodos((prevTodods) => {
+      return prevTodods.concat(newTodo);
+    });
+  };
   return (
     <div>
       {/* <Todos items={['Learn React','Learn yeash']}/> */}
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
